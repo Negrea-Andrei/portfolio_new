1cf9513f101f33f6967471ab4addcabfe6409f3c
Negrea-Andrei <negrea97@yahoo.com>
2024-01-09 21:49:58 +0200

    finished the skill section / change the background on the hero section

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,4 +1,15 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import Carousel from 'react-multi-carousel';
+import web_development from "../assets/img/icons/web_development_logo.svg"
+import react_logo from "../assets/img/icons/react_logo.svg"
+import javascript_logo from "../assets/img/icons/javascript_logo.svg"
+import git_logo from "../assets/img/icons/git_logo.svg"
+import sql_logo from "../assets/img/icons/sql_logo.svg"
+import mongodb_logo from "../assets/img/icons/mongodb_logo.svg"
+import node_js_logo from "../assets/img/icons/node_js_logo.svg"
+import jira_logo from "../assets/img/icons/jira_logo.svg"
+import webpack_logo from "../assets/img/icons/webpack_logo.svg"
+import background1 from "../assets/img/color-sharp.png"
 import 'react-multi-carousel/lib/styles.css';
 
 export default function SkillsSection() {
@@ -22,7 +33,57 @@ export default function SkillsSection() {
         }
       };
     return(
-        <>
-        </>
+        <section className='skill' id='skills'>
+            <Container>
+                <Row> 
+                    <Col>
+                    <div className='skill-bx'>
+                        <h2>
+                            Skills
+                        </h2>
+                        <Carousel responsive={responsive} infinite={true} className='skill-slider'>
+                        <div className='item'>
+                                <img src={web_development} alt='web_development_logo'/>
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={react_logo} alt='react_logo'/>
+                                <h5>React</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={javascript_logo} alt='javascript'/>
+                                <h5>Javascript</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={webpack_logo} alt='webpack_logo'/>
+                                <h5>Webpack</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={node_js_logo} alt='node_js'/>
+                                <h5>Node.JS</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={mongodb_logo} alt='mongodb_logo'/>
+                                <h5>MongoDB</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={sql_logo} alt='sql_logo'/>
+                                <h5>MicrosoftSQL</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={jira_logo} alt='jira_logo_logo'/>
+                                <h5>Jira</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={git_logo} alt='git_logo'/>
+                                <h5>Git</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className='background-image-left' src={background1} alt="skills-background"/>
+        </section>
     )
 }
