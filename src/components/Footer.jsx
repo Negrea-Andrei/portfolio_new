1414234b13db032fceccea8ad146eb767c30f742
Negrea-Andrei <negrea97@yahoo.com>
2024-01-14 18:51:25 +0200

    made changes to projects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg"
+
 export default function Footer() {
-    return(
-        <div>
-        </div>
-    )
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/andrei-negrea-49a669239/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/Negrea-Andrei"><img src={navIcon2} alt="Icon" /></a>
+            </div>
+            <p>© 2023 Negrea Ioan Andrei. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,50 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Tab from "react-bootstrap/Tab";
 import Card from "./Card";
-import placeholder from "../assets/img/placeholder.png"
+import pokemon from "../assets/img/pokemon-game.png";
+import CV from "../assets/img/CV.jpg";
+import placeholder from "../assets/img/placeholder.png";
+import TODO from "../assets/img/TODO.png";
 
 export default function Project() {
   const projects = [
     {
-      title: "Project",
-      description: "description",
-      imgURL: placeholder,
+      title: "Pokemon game",
+      description:
+        "The Pokemon Memory Game is a web application built using Next.js, React, and the PokeAPI. It provides a fun and interactive way for users to test their memory by matching Pokemon images in a grid.",
+      imgURL: pokemon,
+      URL: "https://pokemon-memory-game-pi.vercel.app/",
+    },
+    {
+      title: "Resume maker",
+      description:
+        "The CV App is a web application built using Vite and React that allows users to easily create and edit their professional CVs. Users can input their general information, add a short description, list their educational background, work experience, and skills to generate a well-structured CV.",
+      imgURL: CV,
+      URL: "https://cv-app-fawn.vercel.app/",
+    },
+    {
+      title: "Todo list",
+      description: "The TODO App is a web application designed to help users efficiently manage their projects and tasks. The application provides a straightforward interface for creating, viewing, and organizing projects and associated tasks.",
+      imgURL: TODO,
+      URL: "https://negrea-andrei.github.io/TODO/",
     },
     {
       title: "Project",
-      description: "description",
+      description: "Coming soon",
       imgURL: placeholder,
+      URL: "https://www.youtube.com/watch?v=YSNsZQf_CPM",
     },
     {
       title: "Project",
-      description: "description",
+      description: "Coming soon",
       imgURL: placeholder,
+      URL: "https://www.youtube.com/watch?v=YSNsZQf_CPM",
     },
     {
       title: "Project",
-      description: "description",
+      description: "Coming soon",
       imgURL: placeholder,
+      URL: "https://www.youtube.com/watch?v=YSNsZQf_CPM",
     },
   ];
   return (
@@ -33,13 +54,12 @@ export default function Project() {
           <Col>
             <h2>Projects</h2>
             <p>Here are some of my projects</p>
-            <Tab.Container id="projects-tabs" defaultActiveKey="first">           
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
               <Tab.Content>
                 <Tab.Pane eventKey="first">
                   <Row>
                     {projects.map((project, index) => {
-                        return(
-                      <Card key={index} {...project} />);
+                      return <Card key={index} {...project} />;
                     })}
                   </Row>
                 </Tab.Pane>
