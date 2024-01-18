2e40a8867ae5ff1189ce5995b63a9bec867d3286
Negrea-Andrei <negrea97@yahoo.com>
2024-01-18 20:09:02 +0200

    try other port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, "../build")))
